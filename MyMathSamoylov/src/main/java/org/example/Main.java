a0c2f16c35ab801d8f9b82d1b802bf0dc84a19d0
ForceMajorPro <117585504+ForceMajorPro@users.noreply.github.com>
2023-09-07 14:13:34 +0300

    Add files via upload

---
+++ MyMathSamoylov/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        MyMath Min = new MyMath();
+        int a = 300;
+        int b = 400;
+        System.out.println(Min.returnMin(a, b));
+    }
+
+}
---
+++ MyMathSamoylov/src/main/java/org/example/MyMath.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class MyMath
+{
+    double PI=3.141;
+    double e=2.718;
+    public int returnMin(int a, int b)
+    {
+        if (a>b)
+
+        {return b;}
+
+        else
+        {
+            return a;
+        }
+    }
+}
